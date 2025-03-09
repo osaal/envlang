d551534ad3a3d3c5f530590d5cc08ece91186280
osaal <29450366+osaal@users.noreply.github.com>
2025-03-09 17:58:15 +0200

    feat(AST): Add AST node enumeration

--- src/parser/astnode.rs
+++ src/parser/astnode.rs
@@ -1,2 +1,58 @@
+use std::rc::Rc;
+use crate::environment::EnvScope;
+
 #[derive(Debug, PartialEq)]
-pub enum AstNode {}
+pub enum AstNode {
+    // Literals
+    Integer(isize),
+    Float(f64),
+    Boolean(bool),
+    String(Rc<str>),
+    Identifier(Rc<str>),
+
+    // Environments
+    Environment {
+        name: Option<Rc<str>>, // None for anonymous
+        bindings: Vec<Rc<AstNode>>,
+        parent: Option<Rc<AstNode>>,
+        scope: EnvScope,
+    },
+
+    // Expressions
+    BinaryOp {
+        left: Rc<AstNode>,
+        operator: String,
+        right: Rc<AstNode>,
+    },
+
+    // Bindings
+    Let {
+        name: Rc<str>,
+        value: Rc<AstNode>,
+    },
+
+    // Inheritance
+    Inherit {
+        source: Rc<AstNode>,
+        names: Vec<Rc<str>>,
+    },
+
+    // Functions
+    Function {
+        params: Vec<Rc<str>>,
+        body: Box<AstNode>,
+    },
+    FunctionCall {
+        callee: Box<AstNode>,
+        arguments: Vec<Rc<AstNode>>,
+    },
+}
+
+impl AstNode {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
